11ce737e7e3f53289677af3dff8734cffc40f0e6
M,Ibad khan <160883583+IbadKhan213@users.noreply.github.com>
2024-03-17 01:08:53 +0500

    These are ts and js files

---
+++ main.js
@@ -0,0 +1,108 @@
+// Youtube Session-7.   08-03-2024.  Friday.
+//OPERATORS:
+//1.Arithmetic Operators:Mathematicaly Operators:
+/*
+let num1 = 10;
+let num2 = 4;
+
+//Addition(+):
+let add = num1 + num2;
+
+//Subtraction(-):
+let sub = num1 - num2;
+
+//Multiplication(*):
+let mult = num1 * num2;
+
+//Division(/);
+let div = num1 / num2;
+
+//Modulus(%): means reminder:
+let mod = num1 % num2;
+
+
+//  Operation Perform
+console.log(add);
+console.log(sub);
+console.log(mult);
+console.log(div);
+console.log(mod);
+*/
+//2.Relational / Comparision Operators :
+//In realational operators always answer in boolean.  
+/*
+let num3 = 16;
+let num4 = 20;
+
+//Double Equal to (==)
+let equalto1 = num3==num4;
+let equalto2 = num3==num3;
+
+console.log(equalto1);
+console.log(equalto2);
+
+
+//Triple Equal to (===)
+let tripleEqualto1 = num3===num4;
+let tripleEqualto2 = num3===num3;
+
+console.log(tripleEqualto1);
+console.log(tripleEqualto2);
+ 
+
+//Not Equal to (!==)
+
+let notEqualto1 = num3!==num4;
+let notEqualto2 = num3!==num3;
+
+console.log(notEqualto1);
+console.log(notEqualto2);
+
+
+//Less than (<)
+let lessThan1 = num3<num4;
+let lessThan2 = num4<num3;
+
+console.log(lessThan1);
+console.log(lessThan2);
+
+
+//Less than and Equal to (<=)
+let lessThanAndEqualTo1 = num3<=num4;
+let lessThanAndEqualTo2 = num4<=num3;
+
+console.log(lessThanAndEqualTo1);
+console.log(lessThanAndEqualTo2);
+
+
+//Greater than (>)
+let greaterThan1 = num3>num4;
+let greaterThan2 = num4>num3;
+
+console.log(greaterThan1);
+console.log(greaterThan2);
+
+
+//Greater than avd Equal to (>=)
+let greaterThanAndEqualTo1 = num3>=num4;
+let greaterThanAndEqualTo2 = num4>=num3;
+
+console.log(greaterThanAndEqualTo1);
+console.log(greaterThanAndEqualTo2);
+
+*/
+//HOMEWORK 
+//Q:1(a)
+/*
+let bike = "unique";
+
+console.log('Is bike = "unique" ? I predict True ')
+console.log(bike=="unique");
+*/
+//Q:1(b)
+/*
+let age = 17;
+
+console.log('Is age > 17 ? I predict True ')
+console.log(age>17);
+*/ 
